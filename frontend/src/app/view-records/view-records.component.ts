6870b5a3ea030ddd895206ddd526ceed1c772303
Himansi Vihanga <64607481+Vihanga07@users.noreply.github.com>
2020-06-13 19:12:08 +0530

    Updated-View_Records-ts

--- frontend/src/app/view-records/view-records.component.ts
+++ frontend/src/app/view-records/view-records.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
+export interface ViewRecords {
+  name: string;
+  position: number;
+  description: string;
+  status: string;
+    details: string;
+}
+
+const PATIENT_DATA: ViewRecords[] = [
+  {position: 1, name: 'Nimal', description: 'dskdjf', status: 'Not Admitted', details: 'edit'},
+  {position: 2, name: 'Amal', description: 'ggfg', status: 'Admitted', details: 'edit'},
+  {position: 3, name: 'Kamal', description: 'dgh', status: 'Admitted', details: 'edit'},
+  {position: 4, name: 'Sunil', description: 'ggfdh', status: 'Not Admitted', details: 'edit'},
+  {position: 5, name: 'Rani', description: 'dhadgafa', status: 'Admitted', details: 'edit'}
+ 
+];
+
+/**
+ * @title Basic use of `<mat-table>` (uses display flex)
+ */
 @Component({
-  selector: 'app-view-records',
-  templateUrl: './view-records.component.html',
-  styleUrls: ['./view-records.component.css']
+  selector: 'table-basic-flex-example',
+  styleUrls: ['table-basic-flex-example.css'],
+  templateUrl: 'table-basic-flex-example.html',
 })
-export class ViewRecordsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class TableBasicFlexExample {
+  displayedColumns: string[] = ['position', 'name', 'description', 'status', 'details'];
+  dataSource = PATIENT_DATA;
 }
