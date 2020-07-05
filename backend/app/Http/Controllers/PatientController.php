63d541509f6dd134ce213eafb8d1da98815f2077
IshanSandeepa <64356736+IshanSandeepa@users.noreply.github.com>
2020-07-05 22:13:42 +0530

    Update PatientController.php

--- backend/app/Http/Controllers/PatientController.php
+++ backend/app/Http/Controllers/PatientController.php
@@ -7,13 +7,17 @@ use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use PhpParser\Node\Stmt\Catch_;
 
-//class PatientController extends Controller
+class PatientController extends Controller
 {
-    //
+   
 
     public function register(Request $request)
-    {
+    {   
+        try{
+                 $patient = Patient::create($request->all());
+                 return response()->json(['msg'=>'Patient Created','patient' => $patient]);
+             }catch(QueryException $e){
+                 return response()->json(['msg'=>'Failed','err' => $e->getMessage()]);
 
-       //example
     }
 }
