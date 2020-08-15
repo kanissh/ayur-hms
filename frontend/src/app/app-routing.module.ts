558ee23246be50c6646100a60012a8fd9dff23d0
indeewaribalasooriya <64321778+indeewaribalasooriya@users.noreply.github.com>
2020-08-15 12:04:46 -0700

    Updated app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 // import { LoginPageComponent } from './login-page/login-page.component';
 import { PatientRegComponent } from './patient-reg/patient-reg.component';
+import { SearchPageComponent } from './search-page/search-page.component';
 
 
 const routes: Routes = [
-  { path: '', component: PatientRegComponent },
-  // { path: 'login', component: LoginPageComponent }
+  //{ path: '', component: PatientRegComponent },
+  { path: '', component: SearchPageComponent }
 
 ];
 
