4084d34195285ade5aa400c8d4e3647e154b0c37
kanissh <kanissh11@gmail.com>
2020-09-15 00:06:23 +0530

    Routing update

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 // import { LoginPageComponent } from './login-page/login-page.component';
 import { PatientRegComponent } from './patient-reg/patient-reg.component';
 import { PatientAddmissionComponent } from './patient-addmission/patient-addmission.component';
+import { SearchPageComponent } from './search-page/search-page.component';
 
 
 const routes: Routes = [
   { path: '', component: PatientRegComponent },
-  //{ path: '', component: SearchPageComponent }
+  // { path: '', component: SearchPageComponent }
 ];
 
 @NgModule({
